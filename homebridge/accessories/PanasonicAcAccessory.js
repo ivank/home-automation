cf05d984e643e87e3f947c16463a36fcf0bc8ad4
Ivan Kerin <ikerin@gmail.com>
2017-02-03 00:31:14 +0200

    Implement full thermostat

--- homebridge/accessories/PanasonicAcAccessory.js
+++ homebridge/accessories/PanasonicAcAccessory.js
@@ -1,46 +1,51 @@
 const child_process = require('child_process');
 
 module.exports = function PanasonicAcAccessory (homebridge, logger, config) {
-    const power = new homebridge.hap.Service.Switch()
-        .setCharacteristic(homebridge.hap.Characteristic.Name, 'AC Power');
+    const Characteristic = homebridge.hap.Characteristic;
+    const Service = homebridge.hap.Service;
 
-    const information = new homebridge.hap.Service.AccessoryInformation()
-        .setCharacteristic(homebridge.hap.Characteristic.Name, config.name)
-        .setCharacteristic(homebridge.hap.Characteristic.Manufacturer, 'Panasonic')
-        .setCharacteristic(homebridge.hap.Characteristic.Model, '1.0.0')
-        .setCharacteristic(homebridge.hap.Characteristic.SerialNumber, 'A75C3077');
+    const information = new Service.AccessoryInformation()
+        .setCharacteristic(Characteristic.Manufacturer, 'Panasonic')
+        .setCharacteristic(Characteristic.Model, '1.0.0')
+        .setCharacteristic(Characteristic.SerialNumber, 'A75C3077');
 
-    const thermostat = new homebridge.hap.Service.Thermostat('Panasonic AC');
+    const thermostat = new Service.Thermostat('Panasonic AC').setCharacteristic(Characteristic.Name, 'Panasonic AC');
+
+    const targetTemprature = thermostat.getCharacteristic(Characteristic.TargetTemperature).setProps({ minValue: 16, maxValue: 30 });
+    const targetState = thermostat.getCharacteristic(Characteristic.TargetHeatingCoolingState);
 
     function update () {
-        const isActive = power.getCharacteristic(homebridge.hap.Characteristic.On).value;
-        const temprature = thermostat.getCharacteristic(homebridge.hap.Characteristic.TargetTemperature).value;
+        const state = targetState.getValue();
+        const temprature = targetTemprature.getValue();
+
+        let powerFlag = '';
+        let modeFlag = '';
+
+        if (state === Characteristic.TargetHeatingCoolingState.OFF) {
+            powerFlag = '-x';
+        } else if (state === Characteristic.TargetHeatingCoolingState.AUTO) {
+            modeFlag = '-m AUTO';
+        } else if (state === Characteristic.TargetHeatingCoolingState.COOL) {
+            modeFlag = '-m COOL';
+        } else if (state === Characteristic.TargetHeatingCoolingState.HEAT) {
+            modeFlag = '-m HEAT';
+        }
 
         const tempratureFlag = '-t ' + temprature;
-        const isActiveFlag = isActive ? '' : '-x';
 
-        console.log(temprature);
-
-        child_process.exec(`sudo ~/home-automation/control/control ${isActiveFlag} ${tempratureFlag}`, function (error, stdout, stderr) {
+        child_process.exec(`sudo ~/home-automation/control/control ${powerFlag} ${modeFlag} ${tempratureFlag}`, function (error, stdout, stderr) {
             console.log(error);
             console.log(stderr);
             logger(`stdout!! ${stdout}`);
         });
     }
 
-    power.getCharacteristic(homebridge.hap.Characteristic.On).on('change', update);
-    thermostat.getCharacteristic(homebridge.hap.Characteristic.TargetTemperature).on('change', update);
+    targetTemprature.on('change', update);
+    targetState.on('change', update);
 
     return {
-        name: config.name,
-
-        identify: function (callback) {
-            logger('IDENTIFIED');
-            callback(null);
-        },
-
         getServices: function () {
-            return [power, information, thermostat];
+            return [information, thermostat];
         },
     };
 }
