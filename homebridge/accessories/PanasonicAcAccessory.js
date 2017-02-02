edf231cb2b0bb0a42bb8b72f0b2419a844459891
Ivan Kerin <ikerin@gmail.com>
2017-02-03 00:48:45 +0200

    Fix stuff

--- homebridge/accessories/PanasonicAcAccessory.js
+++ homebridge/accessories/PanasonicAcAccessory.js
@@ -10,13 +10,17 @@ module.exports = function PanasonicAcAccessory (homebridge, logger, config) {
         .setCharacteristic(Characteristic.SerialNumber, 'A75C3077');
 
     const thermostat = new Service.Thermostat('Panasonic AC').setCharacteristic(Characteristic.Name, 'Panasonic AC');
+    thermostat.getCharacteristic(Characteristic.TargetTemperature)
+        .setProps({ value: 25 })
+        .setProps({ minValue: 16, maxValue: 30 });
 
     function update () {
-        const state = thermostat.getCharacteristic(Characteristic.TargetTemperature).value;
-        const temprature = thermostat.getCharacteristic(Characteristic.TargetHeatingCoolingState).value;
+        const state = thermostat.getCharacteristic(Characteristic.TargetHeatingCoolingState).value;
+        const temprature = thermostat.getCharacteristic(Characteristic.TargetTemperature).value;
 
         let powerFlag = '';
         let modeFlag = '';
+        let profileFlag = '';
 
         if (state === Characteristic.TargetHeatingCoolingState.OFF) {
             powerFlag = '-x';
@@ -24,13 +28,15 @@ module.exports = function PanasonicAcAccessory (homebridge, logger, config) {
             modeFlag = '-m AUTO';
         } else if (state === Characteristic.TargetHeatingCoolingState.COOL) {
             modeFlag = '-m COOL';
+            profileFlag = '-p';
         } else if (state === Characteristic.TargetHeatingCoolingState.HEAT) {
             modeFlag = '-m HEAT';
+            profileFlag = '-p';
         }
 
         const tempratureFlag = '-t ' + temprature;
 
-        child_process.exec(`sudo ~/home-automation/control/control ${powerFlag} ${modeFlag} ${tempratureFlag}`, function (error, stdout, stderr) {
+        child_process.exec(`sudo ~/home-automation/control/control ${powerFlag} ${profileFlag} ${modeFlag} ${tempratureFlag}`, function (error, stdout, stderr) {
             console.log(error);
             console.log(stderr);
             logger(`stdout!! ${stdout}`);
