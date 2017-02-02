2754c94e57fcc0d5d6c699335019bdd276304a23
Ivan Kerin <ikerin@gmail.com>
2017-02-02 23:40:31 +0200

    Add information service

--- homebridge/accessories/PanasonicAcAccessory.js
+++ homebridge/accessories/PanasonicAcAccessory.js
@@ -1,10 +1,16 @@
 const child_process = require('child_process');
 
 module.exports = function PanasonicAcAccessory (homebridge, logger, config) {
-    const service = new homebridge.hap.Service.HeaterCooler('Panasonic AC');
+    const information = new homebridge.hap.Service.AccessoryInformation()
+        .setCharacteristic(Characteristic.Name, config.name)
+        .setCharacteristic(Characteristic.Manufacturer, 'Panasonic')
+        .setCharacteristic(Characteristic.Model, '1.0.0')
+        .setCharacteristic(Characteristic.SerialNumber, 'A75C3077');
+
+    const control = new homebridge.hap.Service.HeaterCooler('Panasonic AC');
 
     function update () {
-        const isActive = service.getCharacteristic(homebridge.hap.Characteristic.Active).value;
+        const isActive = control.getCharacteristic(homebridge.hap.Characteristic.Active).value;
         const isActiveFlag = isActive ? '-x' : '';
         logger(`toggle active!! ${isActive}`);
 
@@ -13,11 +19,18 @@ module.exports = function PanasonicAcAccessory (homebridge, logger, config) {
         });
     }
 
-    service.getCharacteristic(homebridge.hap.Characteristic.Active).on('change', update);
+    control.getCharacteristic(homebridge.hap.Characteristic.Active).on('change', update);
 
     return {
+        name: config.name,
+
+        identify: function (callback) {
+            logger('IDENTIFIED');
+            callback(null);
+        },
+
         getServices: function () {
-            [service],
+            return [information, control];
         },
     };
 }
